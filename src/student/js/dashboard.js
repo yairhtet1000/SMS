426e53508030825796f20cd8dab2566f5b405be4
Yaco-0 <ywint056@gmail.com>
2024-06-09 09:19:26 +0630

    Yaco Fix tailwind components

--- src/student/js/dashboard.js
+++ src/student/js/dashboard.js
@@ -2,6 +2,13 @@ const name_bar = document.getElementById("name-bar");
 const name_bar_r = document.getElementById("name-bar-right");
 let lastScrollTop = 0;
 
+window.addEventListener("load", () => {
+  name_bar.classList.remove("ms-[-600px]");
+  name_bar.classList.add("ms-0");
+  name_bar_r.classList.add("right-[-300px]");
+  name_bar_r.classList.remove("right-0");
+});
+
 window.addEventListener("scroll", () => {
   if (window.scrollY > 130) {
     name_bar_r.classList.add("right-0");
