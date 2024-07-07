596a563cef297e31c2aaebb0154da81761251bf2
Yair Htet <yairhtet.me@gmail.com>
2024-07-07 21:35:50 +0630

    yh fix bg black

--- src/student/js/dashboard.js
+++ src/student/js/dashboard.js
@@ -96,7 +96,7 @@ for (let i = 0; i < courseMaterials.length; i++) {
             <h2 class="text-end text-[11px]">${course.title}</h2>
           </div>
           <div class="p-5  select-none">
-            <h2 class="text-end text-black bg-black ">21 files</h2>
+            <h2 class="text-end text-black">21 files</h2>
           </div>
         </div>
         `;
